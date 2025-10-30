dbbcc35cc1e4bd827fc8893d92ae7aace12c5eca
Pr8222 <parsa.rohani82@gmail.com>
2025-10-30 16:46:36 +0330

    Created login.php file and it's functionality.

--- Auth/login.php
+++ Auth/login.php
@@ -1,1 +1,43 @@
 <?php
+
+use Repository\UserRepository;
+
+SESSION_START();
+
+    require_once("../Repository/IUserRepository.php");
+    require_once("../Repository/UserRepository.php");
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $context = new UserRepository();
+        $data = $context->getUserByUsername($_POST['username']);
+        if(empty($data)){
+            $_SESSION['error-message'] = "کاربر یافت نشد!";
+        }
+        if($data['password'] == $_POST['password']){
+            $_SESSION['username'] = $_POST['username'];
+            setcookie("username", $_POST['username'], time() + 3600);
+            header("location: ../User/panel.php");
+            exit;
+        }
+        else{
+            $_SESSION['error-message'] = "رمز عبور اشتباه است!";
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>مدیریت کاربران</title>
+</head>
+<body>
+    <form method="post">
+        <label for="username">نام کاربری</label>
+        <input id="username" type="text" name="username" required> <br>
+        <label for="password">رمز عبور</label>
+        <input id="password" type="password"  name="password" required> <br>
+        <button type="submit">ورود</button>
+    </form>
+</body>
+</html>
